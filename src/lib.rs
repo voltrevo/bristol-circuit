92b46b99af7414561008f22d9803e41c2e10aa45
Andrew Morris <voltrevo@gmail.com>
2024-08-20 15:37:32 +1000

    Expose more things

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,7 @@ mod gate;
 mod raw_bristol_circuit;
 
 pub use bristol_circuit::BristolCircuit;
+pub use bristol_circuit_error::BristolCircuitError;
+pub use circuit_info::{CircuitInfo, ConstantInfo};
+pub use gate::Gate;
+pub use raw_bristol_circuit::RawBristolCircuit;
