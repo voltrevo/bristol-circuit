24eda9f530fb4c92da433ec4e6206b3b70dfb95e
Andrew Morris <voltrevo@gmail.com>
2025-05-02 12:08:28 +1000

    add types

--- src/circuit_info.rs
+++ src/circuit_info.rs
@@ -1,16 +1,43 @@
-use std::collections::BTreeMap;
-
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Default, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct CircuitInfo {
-    pub input_name_to_wire_index: BTreeMap<String, usize>,
-    pub constants: BTreeMap<String, ConstantInfo>,
-    pub output_name_to_wire_index: BTreeMap<String, usize>,
+    pub constants: Vec<ConstantInfo>,
+    pub inputs: Vec<IOInfo>,
+    pub outputs: Vec<IOInfo>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ConstantInfo {
-    pub value: String,
-    pub wire_index: usize,
+    pub name: String,
+
+    #[serde(rename = "type")]
+    pub type_: Value,
+
+    pub address: usize, // aka wire_index
+    pub width: usize,
+
+    pub value: Value,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct IOInfo {
+    pub name: String,
+
+    #[serde(rename = "type")]
+    pub type_: Value,
+
+    pub address: usize, // aka wire_index
+    pub width: usize,
+}
+
+impl CircuitInfo {
+    pub fn input_widths(&self) -> Vec<usize> {
+        self.inputs.iter().map(|input| input.width).collect()
+    }
+
+    pub fn output_widths(&self) -> Vec<usize> {
+        self.outputs.iter().map(|output| output.width).collect()
+    }
 }
